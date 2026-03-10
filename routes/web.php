c40c6a06ffc19282446fff164894bc6163412fe5
dimas pradipto <dpradipto8@gmail.com>
2026-03-10 21:01:26 +0700

    feat: templating form login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'login')->name('login');
+    Route::post('/logout', 'logout')->name('logout');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
