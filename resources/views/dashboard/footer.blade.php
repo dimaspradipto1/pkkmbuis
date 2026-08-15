c10181e340bdae79e9be8929bd4acb0dd8e7c6c7
dimas pradipto <dpradipto8@gmail.com>
2026-08-15 09:28:50 +0700

    feat: add footer component to the dashboard layout

--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -1,6 +1,6 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      <strong><span>PKKMB UIS - HUMAS</span></strong>  &copy; {{ date('Y') }}
+      <strong><span>HUMAS - PKKMB UIS</span></strong>  &copy; {{ date('Y') }}
     </div>
   </footer><!-- End Footer -->
